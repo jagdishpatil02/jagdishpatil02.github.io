2ec0da7cfb9d4fb1a27367071b0ff8ebf0c3ce96
Jagdish Patil <patil.jagdish222@gmail.com>
2024-05-09 11:54:31 +0530

    base url added for app

---
+++ react/winfolio/assets/OuterHeader-CyPkt9WC.js
@@ -0,0 +1,1 @@
+import{j as e,L as s}from"./index-CQubcW8V.js";const r=()=>e.jsx("div",{className:" text-center  py-4",children:e.jsx("span",{className:"self-center whitespace-nowrap underline font-semibold dark:text-white text-2xl",children:e.jsx(s,{to:"/login",className:"cursor-pointer",children:"Winfolio"})})});export{r as O};
